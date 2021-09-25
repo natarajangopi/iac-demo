c1f4441341a7085cd27f73b03a4b928eade90721
gopi natarajan <41574725+natarajangopi@users.noreply.github.com>
2021-09-25 11:25:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
-provider "aws" {
-  region = "us-east-2"
+#configure the aws provider
+provider "aws"{
+ region = "us-east-1"
+ access_key = "AKIAXYLFDOZYEQ5PASWF"
+ secret_key = "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"
+}
+
+
+
+#create a resource for ec2 instance
+resource "aws_instance" "TFuser1_os1"
+{
+ ami = "ami-087c17d1fe0178315"
+ instance_type = "t2.micro"
+ tags = 
+ {
+   Name = "TF"
+ }
 }
