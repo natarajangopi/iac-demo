a912c690011d199b762148706674cebae58143f2
gopi natarajan <41574725+natarajangopi@users.noreply.github.com>
2021-09-25 12:06:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,14 +3,3 @@ provider "aws"{
  region = "us-east-1"
 }
 
-#create a resource for ec2 instance
-resource "aws_instance" "TF_instance"
-{
- ami = "ami-087c17d1fe0178315"
- instance_type = "t2.micro"
- count = "1"
- tags = 
- {
-   Name = "TF"
- }
-}
