3d9a99c1108e2cb09b0de40934eeb5f1701ef3da
gopi natarajan <41574725+natarajangopi@users.noreply.github.com>
2022-05-24 06:32:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,42 @@
-resource "aws_instance" "Tf6" {
- ami = "ami-0022f774911c1d690"
+#create a resource for ec2 instance
+resource "aws_instance" "Tf7" {
+ ami = "ami-09d56f8956ab235b3"
  instance_type = "t2.micro"
  count = "1"
  key_name = "TestJenkins"
  security_groups = ["TF-SG"]
  tags = {
-    Name = "TF6"
+    Name = "TF7"
  }
 }
 
+resource "aws_security_group" "TF-SG12" {
+ name = "TF-SG12"
+ description = "Security group for TF"
+ 
+ ingress{
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ ingress{
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ #outbound from jenkins server
+ egress{
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ tags={
+  Name = "TF-SG12"
+ }
+}
